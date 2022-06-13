8dac61112f55b8c3bf9ab36533df3bb391c50990
Júlio Mendonça da Costa <82334399+juliommen@users.noreply.github.com>
2022-06-13 11:15:55 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://eebe52d6.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
